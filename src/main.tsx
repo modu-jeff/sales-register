26ef720dbfd3807b6fb4b7dbe64c31faee8b28ac
jeff <jeff@moduparking.com>
2023-02-27 17:44:21 +0900

    chore: recoil 라이브러리 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from './style/GlobalStyle'
 import Router from './Router'
-import { RecoilRoot } from 'recoil'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <GlobalStyle />
-      <Router />
-    </RecoilRoot>
+    <GlobalStyle />
+    <Router />
   </React.StrictMode>
 )
